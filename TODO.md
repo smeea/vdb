3887d0c4f14a5ee04612654cb81cf7bc085a1aee
Vadim Matveev <smeea@riseup.net>
2020-09-07 22:29:32 +0300

    update

--- TODO.md
+++ TODO.md
@@ -9,9 +9,12 @@
 * Export/Import (formats: TWD, LackeyCCG) deck
 * Temporary 'add to compare' button
 * Card rulings
-# IMPORTNANT FIX
+# MAJOR FIX
 * Share deck for non-logged users
 * Fix redraw on activeDeck / addMode change
-# MAJOR FIX
+* DeckPreview popover no text
 # MINOR FIX
+* Sort to first show items with sorted attribute
+* DeckPreview to use Result components
 * Fix uncontrolled input bug in clear button
+* Remove unused imports
--- react-frontend/pages/components/DeckPreview.jsx
+++ react-frontend/pages/components/DeckPreview.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import DeckPreviewCrypt from './DeckPreviewCrypt.jsx';
 import DeckPreviewLibrary from './DeckPreviewLibrary.jsx';
@@ -22,14 +22,14 @@ function DeckPreview(props) {
 
   if (props.deck !== undefined) {
     return (
-      <div className='col-md-12 col-lg-10 col-xl-8 px-0 px-xl-2'>
+      <>
         <DeckPreviewCrypt deckCardChange={deckCardChange} deckid={props.deck.deckid} cards={props.deck.crypt} />
         <DeckPreviewLibrary deckCardChange={deckCardChange} deckid={props.deck.deckid} cards={props.deck.library} />
-      </div>
+      </>
     );
   } else {
     return (
-      <React.Fragment></React.Fragment>
+      <></>
     );
   };
 }
