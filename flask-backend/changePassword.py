b2dc8a46570bbc50f32760bbd06644eb07baed00
Vadim Matveev <smeea@riseup.net>
2021-03-22 13:02:22 +0200

    add change password script

---
+++ flask-backend/changePassword.py
@@ -0,0 +1,14 @@
+from api import db
+from models import User, Deck
+from sys import argv
+
+user = User.query.filter_by(username=argv[1]).first()
+
+if len(argv) == 3:
+    user.set_password(argv[2])
+    db.session.commit()
+    print('new password:', argv[2])
+else:
+    print('name:', user.username)
+    print('email:', user.email)
+    print(user.decks.all())
