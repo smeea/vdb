6fd1251cc5d3c09fff2675b7144dc558cb02aaa3
Vadim Matveev <smeea@riseup.net>
2021-06-18 01:30:43 +0300

    add script for cards history in twda

---
+++ flask-backend/createTwdCards.py
@@ -0,0 +1,145 @@
+import json
+
+sets = {
+    "V5": 2020,
+    "25th": 2019,
+    "FB": 2019,
+    "SP": 2019,
+    "KoTR": 2018,
+    "HttBR": 2018,
+    "Anthology": 2018,
+    "LK": 2018,
+    "AU": 2016,
+    "TU": 2014,
+    "DM": 2013,
+    "HttB": 2010,
+    "EK": 2009,
+    "BSC": 2008,
+    "KoT": 2008,
+    "TR": 2008,
+    "SoC": 2007,
+    "LotN": 2007,
+    "NoR": 2006,
+    "Third": 2006,
+    "KMW": 2005,
+    "LoB": 2005,
+    "Gehenna": 2004,
+    "Tenth": 2004,
+    "Anarchs": 2003,
+    "BH": 2003,
+    "CE": 2002,
+    "BL": 2001,
+    "FN": 2001,
+    "SW": 2000,
+    "Sabbat": 1996,
+    "AH": 1996,
+    "DS": 1995,
+    "VTES": 1995,
+    "Jyhad": 1994,
+}
+
+with open("twda.json",
+          "r") as twda_input, open("vtescrypt.json", "r") as crypt_file, open(
+              "vteslib.json", "r") as library_file:
+
+    vtescrypt = json.load(crypt_file)
+    vteslib = json.load(library_file)
+    crypt = {}
+    library = {}
+
+    for card in vtescrypt:
+        crypt[card['Id']] = {
+            'name': card['Name'] if not card['Adv'] else card['Name'] + ' ADV',
+            'twd': '-',
+            'twd_date': '-',
+        }
+
+        date = None
+        for set in card['Set']:
+            if set == 'POD':
+                continue
+
+            d = None
+            if set == 'Promo':
+                d = int(list(card['Set']['Promo'].keys())[0][:4])
+            else:
+                d = sets[set]
+
+            if date is None:
+                date = d
+            elif date > d:
+                date = d
+
+        crypt[card['Id']]['release_date'] = date
+
+    for card in vteslib:
+        library[card['Id']] = {
+            'name': card['Name'],
+            'twd': '-',
+            'twd_date': '-',
+        }
+
+        date = None
+        for set in card['Set']:
+            if set == 'POD':
+                continue
+
+            d = None
+            if set == 'Promo':
+                d = int(list(card['Set']['Promo'].keys())[0][:4])
+            else:
+                d = sets[set]
+
+            if date is None:
+                date = d
+            elif date > d:
+                date = d
+
+        library[card['Id']]['release_date'] = date
+
+    twda = json.load(twda_input)
+    total = len(twda)
+
+    for idx, i in enumerate(twda):
+        # if idx == 0:
+        #     break
+        # print(f"Generating TWDA cards history: {idx + 1} of {total}")
+        deckid = i['id']
+        date = int(i['date'][:4])
+
+        for card in i['crypt']['cards']:
+            crypt[card['id']]['twd'] = deckid
+            crypt[card['id']]['twd_date'] = date
+
+        for cardtype in i['library']['cards']:
+            for card in cardtype['cards']:
+                library[card['id']]['twd'] = deckid
+                library[card['id']]['twd_date'] = date
+
+    print("{:<35} {:<5} {:<5} {:<5} {}".format('Name', 'Print', 'Win', 'YtW',
+                                               'Link'))
+    for c in crypt.values():
+        ytw = None
+        if c['twd_date'] != '-':
+            ytw = c['twd_date'] - c['release_date']
+        else:
+            ytw = str(2021 - c['release_date']) + '+'
+
+        print("{:<35} {:<5} {:<5} {:<4} {}".format(
+            c['name'][:35], c['release_date'], c['twd_date'], ytw,
+            f"https://vdb.smeea.casa/decks?id={c['twd']}"
+            if c['twd'] != '-' else '-'))
+
+    print("{:<35} {:<5} {:<5} {:<5} {}".format('Name', 'Print', 'Win', 'YtW',
+                                               'Link'))
+    for c in library.values():
+        ytw = None
+        if c['twd_date'] != '-':
+            ytw = c['twd_date'] - c['release_date']
+        else:
+            ytw = str(2021 - c['release_date']) + '+'
+
+        print("{:<35} {:<5} {:<5} {:<4} {}".format(
+            c['name'][:35], c['release_date'], c['twd_date'], ytw,
+            f"https://vdb.smeea.casa/decks?id={c['twd']}"
+            if c['twd'] != '-' else '-'))
--- flask-backend/createTwdDecks.py
+++ flask-backend/createTwdDecks.py
@@ -2,7 +2,10 @@ import json
 from searchCryptComponents import get_crypt_by_id
 from searchLibraryComponents import get_library_by_id
 
-with open("twda.json", "r") as twda_input, open("twdDecks.json", "w") as twdaDecks_file, open("twdDecksById.json", "w") as twdaDecksById_file:
+with open("twda.json", "r") as twda_input, open("twdDecks.json",
+                                                "w") as twdaDecks_file, open(
+                                                    "twdDecksById.json",
+                                                    "w") as twdaDecksById_file:
     twda = json.load(twda_input)
     decks = []
     decks_by_id = {}
@@ -20,8 +23,10 @@ with open("twda.json", "r") as twda_input, open("twdDecks.json", "w") as twdaDec
             'date': i['date'],
             'location': i['place'],
             'score': i['score'] if 'score' in i else 'Unknown',
-            'format': i['tournament_format'] if 'tournament_format' in i else 'Unknown',
-            'players': i['players_count'] if 'players_count' in i else 'Unknown',
+            'format':
+            i['tournament_format'] if 'tournament_format' in i else 'Unknown',
+            'players':
+            i['players_count'] if 'players_count' in i else 'Unknown',
             'player': i['player'] if 'player' in i else 'Unknown',
             'crypt': {},
             'cryptTotal': i['crypt']['count'],
@@ -30,7 +35,7 @@ with open("twda.json", "r") as twda_input, open("twdDecks.json", "w") as twdaDec
             'description': i['comments'] if 'comments' in i else 'x',
             'library': {},
             'libraryTotal': i['library']['count'],
-            'link': i['event_link'] if 'event_link' in i else'',
+            'link': i['event_link'] if 'event_link' in i else '',
             'disciplines': [],
             'cardtypes_ratio': {},
             'timestamp': i['date'],
@@ -83,13 +88,15 @@ with open("twda.json", "r") as twda_input, open("twdDecks.json", "w") as twdaDec
         deck['capacity'] = totalCapacity / totalCryptExAC
 
         for type in i['library']['cards']:
-            deck['cardtypes_ratio'][type['type'].lower()] = type['count'] / deck['libraryTotal']
+            deck['cardtypes_ratio'][
+                type['type'].lower()] = type['count'] / deck['libraryTotal']
 
             for card in type['cards']:
 
-                deck['library'][card['id']] = { 'q': card['count'] }
+                deck['library'][card['id']] = {'q': card['count']}
 
-                card_discipline_entry = get_library_by_id(card['id'])['Discipline']
+                card_discipline_entry = get_library_by_id(
+                    card['id'])['Discipline']
                 if '&' in card_discipline_entry:
                     for discipline in card_discipline_entry.split(' & '):
                         if discipline in cryptDisciplines:
@@ -113,8 +120,11 @@ with open("twda.json", "r") as twda_input, open("twdDecks.json", "w") as twdaDec
     json.dump(decks, twdaDecks_file, indent=4, separators=(',', ':'))
     json.dump(decks_by_id, twdaDecksById_file, indent=4, separators=(',', ':'))
 
-
-with open("twda.json", "r") as twda_input, open("twdLocations.json", "w") as twdaLocations_file, open("twdPlayers.json", "w") as twdaPlayers_file:
+with open("twda.json",
+          "r") as twda_input, open("twdLocations.json",
+                                   "w") as twdaLocations_file, open(
+                                       "twdPlayers.json",
+                                       "w") as twdaPlayers_file:
 
     twda = json.load(twda_input)
     locations = set(())
@@ -132,7 +142,6 @@ with open("twda.json", "r") as twda_input, open("twdLocations.json", "w") as twd
 
         players.add(i['player'])
 
-
     locations = sorted(locations)
     locationsOptions = []
     for i in locations:
@@ -145,5 +154,11 @@ with open("twda.json", "r") as twda_input, open("twdLocations.json", "w") as twd
 
     # json.dump(decks, twda_output, separators=(',', ':'))
     # Use this instead, for output with indentation (e.g. for debug)
-    json.dump(playersOptions, twdaPlayers_file, indent=4, separators=(',', ':'))
-    json.dump(locationsOptions, twdaLocations_file, indent=4, separators=(',', ':'))
+    json.dump(playersOptions,
+              twdaPlayers_file,
+              indent=4,
+              separators=(',', ':'))
+    json.dump(locationsOptions,
+              twdaLocations_file,
+              indent=4,
+              separators=(',', ':'))
