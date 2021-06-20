9a070af4c6b6dd8ef257164d6b15f54eada6aa17
Vadim Matveev <smeea@riseup.net>
2021-06-20 14:20:05 +0300

    fix export all deck to add revision to filename

--- flask-backend/deckExportAll.py
+++ flask-backend/deckExportAll.py
@@ -4,6 +4,7 @@ import json
 import sys
 from deckExport import deckExport
 
+
 def deckExportAll(decks, format):
     mydecks = []
     for d in decks:
@@ -11,9 +12,15 @@ def deckExportAll(decks, format):
             'cards': d.cards,
             'name': d.name,
             'author': d.author.public_name,
+            'branch_name': d.branch_name if d.branch_name else "",
             'description': d.description,
         }
+
         result = deckExport(deck, format)
-        mydecks.append({'name': result['name'], 'format': format, 'deck': result['deck']})
+        mydecks.append({
+            'name': result['name'],
+            'format': format,
+            'deck': result['deck']
+        })
 
     return (mydecks)
