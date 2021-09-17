5252b2bc0a6cb0c050705e35a6ae936202f30648
Vadim Matveev <smeea@riseup.net>
2021-09-17 23:12:04 +0300

    save query in url during page change

--- react-frontend/pages/Navigation.jsx
+++ react-frontend/pages/Navigation.jsx
@@ -12,11 +12,154 @@ import AppContext from '../context/AppContext.js';
 import ThemeContext from '../context/ThemeContext.js';
 import NavMobileMenu from './components/NavMobileMenu.jsx';
 import LanguageSelect from './components/LanguageSelect.jsx';
+import cryptDefaults from './components/forms_data/defaultsCryptForm.json';
+import libraryDefaults from './components/forms_data/defaultsLibraryForm.json';
+import twdDefaults from './components/forms_data/defaultsTwdForm.json';
 
 function Navigation(props) {
-  const { inventoryMode, toggleInventoryMode, isMobile, username } =
-    useContext(AppContext);
+  const {
+    cryptFormState,
+    libraryFormState,
+    twdFormState,
+    inventoryMode,
+    toggleInventoryMode,
+    isMobile,
+    username,
+  } = useContext(AppContext);
   const { isDarkTheme, toggleTheme } = useContext(ThemeContext);
+
+  let twdUrl = '/twd';
+  let cryptUrl = '/crypt';
+  let libraryUrl = '/library';
+
+  if (JSON.stringify(cryptFormState) != JSON.stringify(cryptDefaults)) {
+    const input = JSON.parse(JSON.stringify(cryptFormState));
+
+    const multiSelectForms = [
+      'disciplines',
+      'titles',
+      'group',
+      'traits',
+      'set',
+      'precon',
+    ];
+
+    multiSelectForms.map((i) => {
+      Object.keys(input[i]).forEach(
+        (k) => input[i][k] == 0 && delete input[i][k]
+      );
+    });
+
+    const multiSelectFormsWithMain = ['set', 'precon', 'capacity'];
+
+    multiSelectFormsWithMain.map((i) => {
+      if (input[i][i] == 'any') {
+        delete input[i];
+      }
+    });
+
+    Object.keys(input).forEach(
+      (k) =>
+        (input[k] == 'any' ||
+          !input[k] ||
+          Object.keys(input[k]).length === 0) &&
+        delete input[k]
+    );
+    if (input['capacity'] == null) {
+      delete input['capacitymoreless'];
+    }
+
+    if (Object.keys(input).length !== 0) {
+      cryptUrl = `/crypt?q=${encodeURIComponent(JSON.stringify(input))}`;
+    }
+  }
+
+  if (JSON.stringify(libraryFormState) != JSON.stringify(libraryDefaults)) {
+    const input = JSON.parse(JSON.stringify(libraryFormState));
+
+    const multiSelectForms = ['traits', 'set', 'precon'];
+    multiSelectForms.map((i) => {
+      Object.keys(input[i]).forEach(
+        (k) => input[i][k] == 0 && delete input[i][k]
+      );
+    });
+
+    const andOrForms = ['type', 'discipline'];
+    andOrForms.map((i) => {
+      if (Object.keys(input[i][i]).length === 1 && input[i][i][0] === 'any') {
+        delete input[i];
+      }
+      if (input[i] && input[i][i]) {
+        input[i][i] = input[i][i].filter((j) => j !== 'any');
+      }
+    });
+
+    const multiSelectFormsWithMain = [
+      'set',
+      'precon',
+      'blood',
+      'pool',
+      'capacity',
+    ];
+    multiSelectFormsWithMain.map((i) => {
+      if (input[i][i] == 'any') {
+        delete input[i];
+      }
+    });
+
+    Object.keys(input).forEach(
+      (k) =>
+        (input[k] == 'any' ||
+          !input[k] ||
+          Object.keys(input[k]).length === 0) &&
+        delete input[k]
+    );
+
+    if (Object.keys(input).length !== 0) {
+      libraryUrl = `/library?q=${encodeURIComponent(JSON.stringify(input))}`;
+    }
+  }
+
+  if (JSON.stringify(libraryFormState) != JSON.stringify(twdDefaults)) {
+    const input = JSON.parse(JSON.stringify(twdFormState));
+
+    const multiSelectForms = [
+      'disciplines',
+      'traits',
+      'cardtypes',
+      'date',
+      'players',
+      'capacity',
+      'libraryTotal',
+      'matchInventory',
+    ];
+
+    multiSelectForms.map((i) => {
+      Object.keys(input[i]).forEach(
+        (k) => (input[i][k] == 0 || input[i][k] == 'any') && delete input[i][k]
+      );
+    });
+
+    const cardsForms = ['crypt', 'library'];
+
+    cardsForms.map((i) => {
+      Object.keys(input[i]).forEach((k) => {
+        input[i][k] == -1 && delete input[i][k];
+      });
+    });
+
+    Object.keys(input).forEach(
+      (k) =>
+        (input[k] == 'any' ||
+          !input[k] ||
+          Object.keys(input[k]).length === 0) &&
+        delete input[k]
+    );
+
+    if (Object.keys(input).length !== 0) {
+      twdUrl = `/twd?q=${encodeURIComponent(JSON.stringify(input))}`;
+    }
+  }
 
   return (
     <Navbar sticky="top" variant="dark">
@@ -77,7 +220,7 @@ function Navigation(props) {
               </NavLink>
             </>
           )}
-          <NavLink to="/twd" className="nav-link pr-2 pl-1">
+          <NavLink to={twdUrl} className="nav-link pr-2 pl-1">
             TWD
           </NavLink>
           <NavLink to="/inventory" className="nav-link pr-2 pl-1">
@@ -86,10 +229,10 @@ function Navigation(props) {
           <NavLink to="/decks" className="nav-link pr-2 pl-1">
             Decks
           </NavLink>
-          <NavLink to="/crypt" className="nav-link pr-2 pl-1">
+          <NavLink to={cryptUrl} className="nav-link pr-2 pl-1">
             Crypt
           </NavLink>
-          <NavLink to="/library" className="nav-link pr-2 pl-1">
+          <NavLink to={libraryUrl} className="nav-link pr-2 pl-1">
             Library
           </NavLink>
           <NavLink to="/cards" className="nav-link pr-2 pl-1">
