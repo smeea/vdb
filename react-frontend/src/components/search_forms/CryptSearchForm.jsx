smeea/vdb

commit 6521d0c43b19dede287973adab346eb17958913a

fix change search to default (empty) to empty url in twd/crypt/library

react-frontend/src/components/search_forms/CryptSearchForm.jsx
@@ -174,53 +174,46 @@ function CryptSearchForm(props) {
     const url = `${process.env.API_URL}search/crypt`;
     const input = sanitizeFormState('crypt', cryptFormState);
 
-    if (Object.keys(input).length !== 0) {
-      navigate(`/crypt?q=${encodeURIComponent(JSON.stringify(input))}`);
+    navigate(`/crypt?q=${encodeURIComponent(JSON.stringify(input))}`);
 
-      const options = {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(input),
-      };
+    const options = {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(input),
+    };
 
-      setShowError(false);
-      setSpinnerState(true);
+    setShowError(false);
+    setSpinnerState(true);
 
-      fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => {
-          setShowCryptSearch(false);
-          setSpinnerState(false);
-          const res = data.map((i) => {
-            return cryptCardBase[i];
-          });
-          if (!isMobile) {
-            if (hideMissing && inventoryMode) {
-              setPreresults(() =>
-                res.filter((card) => inventoryCrypt[card.Id])
-              );
-            } else {
-              setPreresults(res);
-            }
-          } else {
-            setCryptResults(res);
-          }
-        })
-        .catch((error) => {
-          setSpinnerState(false);
-          if (isMobile) navigate('/crypt');
-          setCryptResults([]);
-          setPreresults([]);
-          setShowError(true);
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        setShowCryptSearch(false);
+        setSpinnerState(false);
+        const res = data.map((i) => {
+          return cryptCardBase[i];
         });
-    } else {
-      setCryptResults(undefined);
-      setPreresults(undefined);
-    }
+        if (!isMobile) {
+          if (hideMissing && inventoryMode) {
+            setPreresults(() => res.filter((card) => inventoryCrypt[card.Id]));
+          } else {
+            setPreresults(res);
+          }
+        } else {
+          setCryptResults(res);
+        }
+      })
+      .catch((error) => {
+        setSpinnerState(false);
+        if (isMobile) navigate('/crypt');
+        setCryptResults([]);
+        setPreresults([]);
+        setShowError(true);
+      });
   };
 
   useEffect(() => {
@@ -230,11 +223,18 @@ function CryptSearchForm(props) {
   }, [cryptFormState]);
 
   useEffect(() => {
-    if (
-      !isMobile &&
-      (!cryptFormState.text.value || cryptFormState.text.value.length > 2)
-    ) {
-      launchRequest();
+    if (!isMobile) {
+      const input = sanitizeFormState('crypt', cryptFormState);
+      if (Object.keys(input).length === 0) {
+        if (query) {
+          handleClearButton();
+        }
+      } else if (
+        !cryptFormState.text.value ||
+        cryptFormState.text.value.length > 2
+      ) {
+        launchRequest();
+      }
     }
   }, [cryptFormState, hideMissing, inventoryMode]);
 

react-frontend/src/components/search_forms/LibrarySearchForm.jsx
@@ -158,53 +158,48 @@ function LibrarySearchForm(props) {
     const url = `${process.env.API_URL}search/library`;
     const input = sanitizeFormState('library', libraryFormState);
 
-    if (Object.keys(input).length !== 0) {
-      navigate(`/library?q=${encodeURIComponent(JSON.stringify(input))}`);
+    navigate(`/library?q=${encodeURIComponent(JSON.stringify(input))}`);
 
-      const options = {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(input),
-      };
+    const options = {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(input),
+    };
 
-      setShowError(false);
-      setSpinnerState(true);
+    setShowError(false);
+    setSpinnerState(true);
 
-      fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => {
-          setSpinnerState(false);
-          setShowLibrarySearch(false);
-          const res = data.map((i) => {
-            return libraryCardBase[i];
-          });
-          if (!isMobile) {
-            if (hideMissing && inventoryMode) {
-              setPreresults(() =>
-                res.filter((card) => inventoryLibrary[card.Id])
-              );
-            } else {
-              setPreresults(res);
-            }
-          } else {
-            setLibraryResults(res);
-          }
-        })
-        .catch((error) => {
-          if (isMobile) navigate('/library');
-          setSpinnerState(false);
-          setLibraryResults([]);
-          setPreresults([]);
-          setShowError(true);
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        setSpinnerState(false);
+        setShowLibrarySearch(false);
+        const res = data.map((i) => {
+          return libraryCardBase[i];
         });
-    } else {
-      setLibraryResults(undefined);
-      setPreresults(undefined);
-    }
+        if (!isMobile) {
+          if (hideMissing && inventoryMode) {
+            setPreresults(() =>
+              res.filter((card) => inventoryLibrary[card.Id])
+            );
+          } else {
+            setPreresults(res);
+          }
+        } else {
+          setLibraryResults(res);
+        }
+      })
+      .catch((error) => {
+        if (isMobile) navigate('/library');
+        setSpinnerState(false);
+        setLibraryResults([]);
+        setPreresults([]);
+        setShowError(true);
+      });
   };
 
   useEffect(() => {
@@ -214,11 +209,18 @@ function LibrarySearchForm(props) {
   }, [libraryFormState]);
 
   useEffect(() => {
-    if (
-      !isMobile &&
-      (!libraryFormState.text.value || libraryFormState.text.value.length > 2)
-    ) {
-      launchRequest();
+    if (!isMobile) {
+      const input = sanitizeFormState('library', libraryFormState);
+      if (Object.keys(input).length === 0) {
+        if (query) {
+          handleClearButton();
+        }
+      } else if (
+        !libraryFormState.text.value ||
+        libraryFormState.text.value.length > 2
+      ) {
+        launchRequest();
+      }
     }
   }, [libraryFormState, hideMissing, inventoryMode]);
 

react-frontend/src/components/search_forms/TwdSearchForm.jsx
@@ -165,35 +165,33 @@ function TwdSearchForm(props) {
     const url = `${process.env.API_URL}search/twd`;
     const input = sanitizeFormState('twd', twdFormState);
 
-    if (Object.keys(input).length !== 0) {
-      navigate(`/twd?q=${encodeURIComponent(JSON.stringify(input))}`);
+    navigate(`/twd?q=${encodeURIComponent(JSON.stringify(input))}`);
 
-      const options = {
-        method: 'POST',
-        mode: 'cors',
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(input),
-      };
+    const options = {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(input),
+    };
 
-      setShowError(false);
-      setSpinnerState(true);
+    setShowError(false);
+    setSpinnerState(true);
 
-      fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => {
-          setSpinnerState(false);
-          setShowTwdSearch(false);
-          setTwdResults(data);
-        })
-        .catch((error) => {
-          setSpinnerState(false);
-          setTwdResults([]);
-          setShowError(true);
-        });
-    }
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        setSpinnerState(false);
+        setShowTwdSearch(false);
+        setTwdResults(data);
+      })
+      .catch((error) => {
+        setSpinnerState(false);
+        setTwdResults([]);
+        setShowError(true);
+      });
   };
 
   const getNewTwd = (q) => {
@@ -250,8 +248,15 @@ function TwdSearchForm(props) {
   };
 
   useEffect(() => {
-    if (!isMobile && (!twdFormState.event || twdFormState.event.length > 2)) {
-      launchRequest();
+    if (!isMobile) {
+      const input = sanitizeFormState('twd', twdFormState);
+      if (Object.keys(input).length === 0) {
+        if (query) {
+          handleClearButton();
+        }
+      } else if (!twdFormState.event || twdFormState.event.length > 2) {
+        launchRequest();
+      }
     }
   }, [twdFormState]);
 

commit d1796eec2a85b58bdd6de1a82c04df49a0fe233b

fix opening search from url (was broken in 6521d0c)

react-frontend/src/components/search_forms/CryptSearchForm.jsx
@@ -227,7 +227,9 @@ function CryptSearchForm(props) {
       const input = sanitizeFormState('crypt', cryptFormState);
       if (Object.keys(input).length === 0) {
         if (query) {
-          handleClearButton();
+          navigate('/crypt');
+          setCryptResults(undefined);
+          setPreresults(undefined);
         }
       } else if (
         !cryptFormState.text.value ||

react-frontend/src/components/search_forms/LibrarySearchForm.jsx
@@ -213,7 +213,9 @@ function LibrarySearchForm(props) {
       const input = sanitizeFormState('library', libraryFormState);
       if (Object.keys(input).length === 0) {
         if (query) {
-          handleClearButton();
+          navigate('/library');
+          setLibraryResults(undefined);
+          setPreresults(undefined);
         }
       } else if (
         !libraryFormState.text.value ||

react-frontend/src/components/search_forms/TwdSearchForm.jsx
@@ -252,7 +252,8 @@ function TwdSearchForm(props) {
       const input = sanitizeFormState('twd', twdFormState);
       if (Object.keys(input).length === 0) {
         if (query) {
-          handleClearButton();
+          navigate('/twd');
+          setTwdResults(undefined);
         }
       } else if (!twdFormState.event || twdFormState.event.length > 2) {
         launchRequest();