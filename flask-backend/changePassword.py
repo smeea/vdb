61b35bc2d1e87d70eab12e0146ecfe81b237dfbf
Vadim Matveev <smeea@riseup.net>
2022-01-30 13:32:10 +0300

    add password generation to changePassword script

--- flask-backend/changePassword.py
+++ flask-backend/changePassword.py
@@ -1,13 +1,17 @@
 from api import db
 from models import User, Deck
 from sys import argv
+import random
+import string
 
-user = User.query.filter_by(username=argv[1]).first()
+name = argv[1].lower()
+user = User.query.filter_by(username=name).first()
 
 if len(argv) == 3:
-    user.set_password(argv[2])
+    password = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(10))
+    user.set_password(password)
     db.session.commit()
-    print('new password:', argv[2])
+    print('new password:', password)
 else:
     print('name:', user.username)
     print('email:', user.email)
