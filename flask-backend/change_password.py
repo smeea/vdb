a6c0d27ac3350d67780d0abbf370c83f051d7e87
Vadim Matveev <smeea@riseup.net>
2022-03-28 20:03:28 +0300

    update change_password script to accept email as query

--- flask-backend/change_password.py
+++ flask-backend/change_password.py
@@ -1,22 +1,24 @@
 from api import db
-from models import User, Deck
+from models import User
 from sys import argv, exit
 import random
 import string
 
 if len(argv) <= 1:
-    exit('need username as argument')
+    exit("need username as argument")
 
-name = argv[1].lower()
-user = User.query.filter_by(username=name).first()
+q = argv[1].lower()
+user = User.query.filter_by(username=q).first()
+if not user:
+    user = User.query.filter_by(email=q).first()
 
 if len(argv) == 2:
-    print("name:", user.username)
-    print("email:", user.email)
-    print("decks: ", user.decks.all())
+    print("name:  ", user.username)
+    print("email: ", user.email)
+    print("decks: ", [deck.name for deck in user.decks.all()])
     print("inventory: ", len(user.inventory))
 
-elif argv[2] == 'x':
+elif argv[2] == "x":
     password = "".join(
         random.SystemRandom().choice(string.ascii_letters + string.digits)
         for _ in range(10)
