8dd08d1cd5fad724143726b0fec8769ce4caf152
Vadim Matveev <smeea@riseup.net>
2022-10-14 10:03:07 +0400

    fix twd/pda capacity filter

--- frontend/src/components/search_forms/twd_search_components/TwdSearchFormCapacity.jsx
+++ frontend/src/components/search_forms/twd_search_components/TwdSearchFormCapacity.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ButtonGroup, Button } from 'react-bootstrap';
 
 const TwdSearchFormCapacity = ({ value, onChange }) => {
-  const capacityBrackets = ['1-4', '4-6', '6-8', '8- =>11'];
+  const capacityBrackets = ['1-4', '4-6', '6-8', '8-11'];
 
   const CryptCapacityButtons = capacityBrackets.map((i, index) => {
     return (
