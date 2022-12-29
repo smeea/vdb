54ee8d4136c4e135413096b18319163db386a8ac
Vadim Matveev <smeea@riseup.net>
2022-12-29 16:28:15 +0500

    migration to tailwind (modal width)

--- frontend/src/components/misc/Modal.jsx
+++ frontend/src/components/misc/Modal.jsx
@@ -10,6 +10,19 @@ const Modal = ({ handleClose, centered, size = 'md', title, children }) => {
   // TODO unfocus close
   // TODO close with Esc
 
+  const getWidth = (s) => {
+    switch (s) {
+      case 'md':
+        return 'min-w-full sm:min-w-[80%] md:min-w-[65%] lg:min-w-[55%] xl:min-w-[45%] 2xl-min-w-[40%]';
+      case 'lg':
+        return 'min-w-full sm:min-w-full md:min-w-[80%] lg:min-w-[70%] xl:min-w-[60%] 2xl-min-w-[55%]';
+      case 'xl':
+        return 'min-w-full sm:min-w-full md:min-w-[95%] lg:min-w-[85%] xl:min-w-[75%] 2xl-min-w-[70%]';
+    }
+  }
+
+  const widthClass = getWidth(size)
+
   return (
     <Dialog open={true} onClose={handleClose} className="relative z-50">
       <div
@@ -18,14 +31,11 @@ const Modal = ({ handleClose, centered, size = 'md', title, children }) => {
       />
 
       <div
-        className={`fixed inset-0 flex p-0 sm:p-8 ${
-          centered ? 'items-center' : 'items-start'
-        } justify-center overflow-auto`}
+        className={`fixed inset-0 flex p-0 sm:p-8 ${centered ? 'items-center' : 'items-start'
+          } justify-center overflow-auto`}
       >
         <Dialog.Panel
-          className={`${
-            size ? `modal-${size}` : ''
-          } space-y-2 rounded bg-bgPrimary p-5 dark:bg-bgPrimaryDark`}
+          className={`${widthClass} space-y-2 rounded bg-bgPrimary p-5 dark:bg-bgPrimaryDark`}
         >
           {title && (
             <Dialog.Title className="flex items-start justify-between border-none">
