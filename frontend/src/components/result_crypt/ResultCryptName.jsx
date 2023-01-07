f9ba663113d10c1b002e3c5eabff59afd3f81522
Vadim Matveev <smeea@riseup.net>
2023-01-07 20:30:43 +0400

    migration to tailwind

--- frontend/src/components/result_crypt/ResultCryptName.jsx
+++ frontend/src/components/result_crypt/ResultCryptName.jsx
@@ -3,25 +3,27 @@ import Hammer from 'assets/images/icons/hammer.svg';
 
 const ResultCryptName = ({ card }) => {
   return (
-    <div className="inline whitespace-nowrap text-fgName dark:text-fgNameDark">
-      <div className="inline whitespace-normal ">
-        {card['Banned'] ? <strike>{card['Name']}</strike> : <>{card['Name']}</>}
-      </div>
-      {card.Adv[0] && (
-        <>
-          {' '}
+    <div className="inline text-fgName dark:text-fgNameDark space-x-1">
+      <div className="inline whitespace-nowrap space-x-1">
+        <div
+          className={`inline whitespace-normal space-x-1 ${
+            card.Banned ? 'line-through' : ''
+          }`}
+        >
+          {card['Name']}
+        </div>
+        {card.Adv[0] && (
           <img
             className="inline h-[22px] align-text-bottom"
             src={`${process.env.ROOT_URL}images/misc/advanced.svg`}
             title="Advanced"
           />
-        </>
-      )}
+        )}
+      </div>
       {card['Banned'] && (
-        <>
-          {' '}
+        <div className="inline">
           [{card['Banned']} <Hammer className="inline h-[20px]" />]
-        </>
+        </div>
       )}
     </div>
   );
--- frontend/src/components/result_library/ResultLibraryName.jsx
+++ frontend/src/components/result_library/ResultLibraryName.jsx
@@ -3,14 +3,14 @@ import Hammer from 'assets/images/icons/hammer.svg';
 
 const ResultLibraryName = ({ card }) => {
   return (
-    <div className="inline whitespace-nowrap text-fgName dark:text-fgNameDark">
-      {card['Banned'] ? (
-        <>
-          <strike>{card['Name']}</strike> [{card['Banned']}{' '}
-          <Hammer className="inline" />]
-        </>
-      ) : (
-        <>{card['Name']}</>
+    <div className="inline text-fgName dark:text-fgNameDark space-x-1">
+      <div className={`inline ${card.Banned ? 'line-through' : ''}`}>
+        {card['Name']}
+      </div>
+      {card['Banned'] && (
+        <div className="inline">
+          [{card['Banned']} <Hammer className="inline h-[20px]" />]
+        </div>
       )}
     </div>
   );
