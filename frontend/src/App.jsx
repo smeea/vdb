9ff05a833bdbd02f193b89b5e268cd6a90caa578
Vadim Matveev <smeea@riseup.net>
2023-01-29 18:26:19 +0300

    add code splitting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {
   RouterProvider,
   createBrowserRouter,
@@ -11,19 +11,21 @@ import About from './pages/About.jsx';
 import Account from './pages/Account.jsx';
 import Cards from './pages/Cards.jsx';
 import Crypt from './pages/Crypt.jsx';
-import Decks from './pages/Decks.jsx';
-import Diff from './pages/Diff.jsx';
-import Inventory from './pages/Inventory.jsx';
 import Library from './pages/Library.jsx';
-import Review from './pages/Review.jsx';
-import Pda from './pages/Pda.jsx';
-import Twd from './pages/Twd.jsx';
-import Changelog from './pages/Changelog.jsx';
-import Documentation from './pages/Documentation.jsx';
-import TwdHallOfFameCards from './pages/TwdHallOfFameCards.jsx';
-import TwdHallOfFameTournaments from './pages/TwdHallOfFameTournaments.jsx';
-import TwdCardsHistory from './pages/TwdCardsHistory.jsx';
-import TwdCheck from './pages/TwdCheck.jsx';
+import Decks from './pages/Decks.jsx';
+
+const Diff = lazy(() => import('./pages/Diff.jsx'))
+const Inventory = lazy(() => import('./pages/Inventory.jsx'))
+const Review = lazy(() => import('./pages/Review.jsx'))
+const Pda = lazy(() => import('./pages/Pda.jsx'))
+const Twd = lazy(() => import('./pages/Twd.jsx'))
+const Changelog = lazy(() => import('./pages/Changelog.jsx'))
+const Documentation = lazy(() => import('./pages/Documentation.jsx'))
+const TwdHallOfFameCards = lazy(() => import('./pages/TwdHallOfFameCards.jsx'))
+const TwdHallOfFameTournaments = lazy(() => import('./pages/TwdHallOfFameTournaments.jsx'))
+const TwdCardsHistory = lazy(() => import('./pages/TwdCardsHistory.jsx'))
+const TwdCheck = lazy(() => import('./pages/TwdCheck.jsx'))
+
 import { loader as deckLoader } from './pages/Decks.jsx';
 import { loader as changesLoader } from './pages/Changelog.jsx';
 
@@ -33,30 +35,31 @@ const App = () => {
       <Route path="/" element={<RootLayout />}>
         <Route index element={<About />} />
         <Route path="about" element={<Navigate to="/" />} />
-        <Route path="documentation" element={<Documentation />} />
-        <Route path="changelog" element={<Changelog />} loader={changesLoader} />
         <Route path="account" element={<Account />} />
-        <Route path="diff" element={<Diff />} />
-        <Route path="diff/:deckidFrom/:deckidTo" element={<Diff />} />
-        <Route path="inventory" element={<Inventory />} />
-        <Route path="decks" element={<Decks />} />
-        <Route path="/decks/:deckid" element={<Decks />} loader={deckLoader} />
-        <Route path="pda" element={<Pda />} />
-        <Route path="review" element={<Review />} />
-        <Route path="review/:deckid" element={<Review />} />
-        <Route path="twd" element={<Twd />} />
-        <Route path="twd/deck_check" element={<TwdCheck />} />
-        <Route
-          path="twd/hall_of_fame/tournaments"
-          element={<TwdHallOfFameTournaments />}
-        />
-        <Route path="twd/hall_of_fame/cards" element={<TwdHallOfFameCards />} />
-        <Route path="twd/cards_history" element={<TwdCardsHistory />} />
-        <Route path="crypt" element={<Crypt />} />
-        <Route path="library" element={<Library />} />
         <Route path="cards" element={<Cards />}>
           <Route path=":cardid" element={<Cards />} />
         </Route>
+        <Route path="crypt" element={<Crypt />} />
+        <Route path="library" element={<Library />} />
+        <Route path="decks" element={<Decks />} />
+        <Route path="/decks/:deckid" element={<Decks />} loader={deckLoader} />
+
+        <Route path="documentation" element={<Suspense fallback={<div />}><Documentation /></Suspense>} />
+        <Route path="changelog" element={<Suspense fallback={<div />}><Changelog /></Suspense>} loader={changesLoader} />
+        <Route path="diff" element={<Suspense fallback={<div />}><Diff /></Suspense>} />
+        <Route path="diff/:deckidFrom/:deckidTo" element={<Suspense fallback={<div />}><Diff /></Suspense>} />
+        <Route path="inventory" element={<Suspense fallback={<div />}><Inventory /></Suspense>} />
+        <Route path="pda" element={<Suspense fallback={<div />}><Pda /></Suspense>} />
+        <Route path="review" element={<Suspense fallback={<div />}><Review /></Suspense>} />
+        <Route path="review/:deckid" element={<Suspense fallback={<div />}><Review /></Suspense>} />
+        <Route path="twd" element={<Suspense fallback={<div />}><Twd /></Suspense>} />
+        <Route path="twd/deck_check" element={<Suspense fallback={<div />}><TwdCheck /></Suspense>} />
+        <Route
+          path="twd/hall_of_fame/tournaments"
+          element={<Suspense fallback={<div />}><TwdHallOfFameTournaments /></Suspense>}
+        />
+        <Route path="twd/hall_of_fame/cards" element={<Suspense fallback={<div />}><TwdHallOfFameCards /></Suspense>} />
+        <Route path="twd/cards_history" element={<Suspense fallback={<div />}><TwdCardsHistory /></Suspense>} />
       </Route>
     )
   );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,4 +14,7 @@ export default defineConfig({
       '@': require('path').resolve(__dirname, 'src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1024,
+  },
 });
