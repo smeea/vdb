51acab182d6d8d8f8adc12666d8aa1bec3bea6ce
Vadim Matveev <smeea@riseup.net>
2023-02-04 23:13:50 +0300

    migrate pwa to vite-pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
@@ -8,6 +9,33 @@ export default defineConfig({
       exportAsDefault: true,
     }),
     react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'VDB',
+        short_name: 'VDB',
+        description:
+          'Card search and deck building app for Vampire the Eternal Struggle (VTES).',
+        lang: 'en-US',
+        start_url: '/crypt',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#404060',
+        icons: [
+          {
+            src: 'icon512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icon512-maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
