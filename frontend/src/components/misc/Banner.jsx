b4e7837c8d9242ff4d688aaf2430bb7196c36d67
Vadim Matveev <smeea@riseup.net>
2023-07-31 22:19:33 -0400

    update about

--- frontend/src/components/misc/Banner.jsx
+++ frontend/src/components/misc/Banner.jsx
@@ -6,7 +6,10 @@ const Banner = () => {
 
   return (
     <div className="flex w-full items-center justify-between border border-borderSecondary bg-bgSecondary p-2 dark:border-borderSecondaryDark dark:bg-bgSecondaryDark">
-      <div onClick={() => navigate('/')} className="flex items-center">
+      <div
+        onClick={() => navigate('/')}
+        className="flex items-center cursor-pointer"
+      >
         <img
           className="dark:brightness-150"
           src={`${import.meta.env.VITE_BASE_URL}/images/misc/logo.svg`}
