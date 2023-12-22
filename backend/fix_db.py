c65ce7a59ab4df4f81bfd9b86542f0d8ae221cc3
Vadim Matveev <smeea@riseup.net>
2023-12-22 17:42:17 -0500

    update database fix script

--- backend/fix_db.py
+++ backend/fix_db.py
@@ -1,38 +1,30 @@
 from api import app, db
 from models import Deck, User
-import json
 
 # NOT REQUIRED; USE ONLY AS TEMPLATE FOR FUTURE FIXES
 
-# fixes = {
-#     210001: 000000,
-# }
-# with app.app_context():
-#     for deck in Deck.query.all():
-#         new_cards = {}
-#         for k, v in deck.cards.items():
-#             if k in fixes:
-#                 new_cards[fixes[k]] = v
-#                 print(f"{k} to {fixes[k]}")
-#             else:
-#                 new_cards[k] = v
-
-#         deck.cards = new_cards
-
-#     db.session.commit()
-
-removes = [ 220003, 220007 ]
+changes = {
+    210001: 000000,
+}
+removes = [ ]
 with app.app_context():
     for deck in Deck.query.all():
-        merged_cards = deck.cards.copy()
+        new_cards = {}
+        for k, v in deck.cards.items():
+            if k in changes:
+                new_cards[changes[k]] = v
+                print(f"{k} to {changes[k]}")
+            else:
+                new_cards[k] = v
+
         for k in removes:
-            if k in merged_cards:
-                del merged_cards[k]
+            if k in new_cards:
+                del new_cards[k]
             if k in deck.used_in_inventory:
                 used_cards = deck.used_in_inventory.copy()
                 del used_cards[k]
                 deck.used_in_inventory = used_cards.copy()
 
-        deck.cards = merged_cards
+        deck.cards = new_cards
 
     db.session.commit()
