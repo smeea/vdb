69f8502d2cf5ac05348695e318ead483d6d1ac78
Vadim Matveev <smeea@riseup.net>
2024-01-10 19:45:25 -0500

    remove deprecated code (redirect didnt work as expected and i dont want to waste time fixing it )

--- frontend/src/pages/Review.jsx
+++ frontend/src/pages/Review.jsx
@@ -36,14 +36,7 @@ const Review = () => {
   const navigate = useNavigate();
   const { deckid } = useParams();
   const { hash } = useLocation();
-  const query = new URLSearchParams(useLocation().search);
   const loaderData = useLoaderData();
-
-  // Redirect from old links
-  if (query.get('id') && hash) {
-    const url = `/review/${deckid}${hash}`;
-    navigate(url);
-  }
 
   const [deckFrom, setDeckFrom] = useImmer();
   const [deckTo, setDeckTo] = useImmer();
@@ -110,7 +103,7 @@ const Review = () => {
   useEffect(() => {
     const diff = getDiff(
       { ...deckFrom?.crypt, ...deckFrom?.library },
-      { ...deckTo?.crypt, ...deckTo?.library },
+      { ...deckTo?.crypt, ...deckTo?.library }
     );
 
     if (Object.keys(diff).length) {
@@ -141,7 +134,7 @@ const Review = () => {
   useEffect(() => {
     if (hash && deckTo) {
       const deckWithHash = JSON.parse(
-        JSON.stringify({ crypt: deckTo.crypt, library: deckTo.library }),
+        JSON.stringify({ crypt: deckTo.crypt, library: deckTo.library })
       );
       hash
         .slice(1)
@@ -198,7 +191,7 @@ const Review = () => {
 
   const parentId = deckFrom?.description.replace(
     `Review of ${import.meta.env.VITE_BASE_URL}/decks/`,
-    '',
+    ''
   );
   const inDecks = decks ? Object.keys(decks).includes(parentId) : null;
 
