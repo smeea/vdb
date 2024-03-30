2c8034584644cd11635ab3e4aa495f397213175d
Vadim Matveev <smeea@riseup.net>
2024-03-29 22:08:25 -0400

    add salut brie! to tournament analyze

--- frontend/src/components/tournament_analyze/AnalyzeLoadPreparedButtons.jsx
+++ frontend/src/components/tournament_analyze/AnalyzeLoadPreparedButtons.jsx
@@ -17,6 +17,31 @@ const AnalyzeLoadPreparedButtons = ({
   const navigate = useNavigate();
   const [loadingDeck, setLoadingDeck] = useState();
 
+  const tournaments = [
+    {
+      id: 11196,
+      title: 'Salut Brie! - Norwich, United Kingdom',
+      date: '2024-03-09',
+    },
+    {
+      id: 11192,
+      title: 'White Turtle - Vác, Hungary',
+      date: '2024-01-27',
+    },
+    {
+      id: 11023,
+      title: 'Finnish Nationals 2023 - Espoo, Finland',
+      date: '2023-11-04',
+      isNC: true,
+    },
+    {
+      id: 10367,
+      title: 'Finnish Nationals 2022 - Espoo, Finland',
+      date: '2022-11-05',
+      isNC: true,
+    },
+  ];
+
   const handlePrepared = (id) => {
     setLoadingDeck(id);
     clearAnalyzeForm();
@@ -31,30 +56,20 @@ const AnalyzeLoadPreparedButtons = ({
 
   return (
     <div className="flex flex-col gap-2">
-      <ButtonIconed
-        className="w-full"
-        variant="primary"
-        onClick={() => handlePrepared(11192)}
-        title="White Turtle - Vác, Hungary"
-        text="White Turtle - Vác, Hungary [2024-01-27]"
-        icon={loadingDeck == 11192 ? <Spinner /> : null}
-      />
-      <ButtonIconed
-        className="w-full"
-        variant="primary"
-        onClick={() => handlePrepared(11023)}
-        title="Finnish Nationals 2023 - Espoo, Finland"
-        icon={loadingDeck == 11023 ? <Spinner /> : <StarFill />}
-        text="Finnish Nationals 2023 - Espoo, Finland [2023-11-04]"
-      />
-      <ButtonIconed
-        className="w-full"
-        variant="primary"
-        onClick={() => handlePrepared(10367)}
-        title="Finnish Nationals 2022 - Espoo, Finland"
-        icon={loadingDeck == 10367 ? <Spinner /> : <StarFill />}
-        text="Finnish Nationals 2022 - Espoo, Finland [2022-11-05]"
-      />
+      {tournaments.map((v) => {
+        return (
+          <ButtonIconed
+            key={v.id}
+            className="w-full"
+            onClick={() => handlePrepared(v.id)}
+            title={v.title}
+            text={`${v.title} [${v.date}]`}
+            icon={
+              loadingDeck == v.id ? <Spinner /> : v.isNC ? <StarFill /> : null
+            }
+          />
+        );
+      })}
     </div>
   );
 };
