2788a24b41e2619b2964fbe9593ae68ebe5d3f23
Vadim Matveev <smeea@riseup.net>
2024-08-09 16:35:44 +0300

    fix eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,51 +1,43 @@
-import react from "eslint-plugin-react";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from 'globals';
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...compat.extends("eslint:recommended", "plugin:react/recommended"), {
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+    ...react.configs.flat.recommended,
     plugins: {
-        react,
+      react,
     },
-
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
+      globals: {
+        ...globals.browser,
+      },
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-
-        ecmaVersion: 2021,
-        sourceType: "module",
-
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
-        },
+      },
     },
-
     settings: {
-        react: {
-            version: "detect",
-        },
+      react: {
+        version: 'detect',
+      },
     },
-
     rules: {
-        "no-empty": ["error", {
-            allowEmptyCatch: true,
-        }],
-
-        "require-jsdoc": "off",
-        "react/prop-types": "off",
+      'no-empty': [
+        'error',
+        {
+          allowEmptyCatch: true,
+        },
+      ],
+      'require-jsdoc': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
     },
-}];
+  },
+];
