c3cbb5b163a23737e34cecb8b4b3e2a49f9d8ab3
Vadim Matveev <smeea@riseup.net>
2024-09-28 02:21:31 +0300

    fix inventory cards to change even-odd bg-colors while scrolling

--- frontend/src/components/misc/WindowRows.jsx
+++ frontend/src/components/misc/WindowRows.jsx
@@ -4,7 +4,7 @@ const WindowRows = ({ index, style, data }) => {
   return (
     <div
       style={style}
-      className="row-bg flex border-b border-bgSecondary dark:border-bgSecondaryDark"
+      className={`${index % 2 ? 'bg-bgThird dark:bg-bgThirdDark' : 'bg-bgPrimary dark:bg-bgPrimaryDark'} flex border-b border-bgSecondary dark:border-bgSecondaryDark`}
     >
       {data[index]}
     </div>
