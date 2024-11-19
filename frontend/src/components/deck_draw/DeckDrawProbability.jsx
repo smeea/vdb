086e0298e6ef478e5da12f26711e1e7115292ab5
Vadim Matveev <smeea@riseup.net>
2024-11-20 02:11:13 +0300

    restyle draw chances for library types

--- frontend/src/components/deck_draw/DeckDrawProbability.jsx
+++ frontend/src/components/deck_draw/DeckDrawProbability.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { ConditionalTooltipOrModal, DeckDrawProbabilityText } from '@/components';
 import { drawProbability } from '@/utils';
+import { useApp } from '@/context';
+import cardtypeSortedFull from '@/assets/data/cardtypeSortedFull.json';
 
 const DeckDrawProbability = ({ cardName, n, N, k }) => {
+  const { setShowFloatingButtons } = useApp();
+
   return (
     <ConditionalTooltipOrModal
       overlay={<DeckDrawProbabilityText N={N} n={n} k={k} />}
       title={cardName}
+      onClick={() => setShowFloatingButtons(false)}
+      onClose={() => setShowFloatingButtons(true)}
       withMobileMargin
     >
-      <div className="text-fgSecondary dark:text-fgSecondaryDark">
+      <div
+        className={
+          cardtypeSortedFull.includes(cardName)
+            ? 'text-fgPrimary dark:text-fgPrimaryDark'
+            : 'text-fgSecondary dark:text-fgSecondaryDark'
+        }
+      >
         {`${Math.round(drawProbability(1, N, n, k) * 100)}%`}
       </div>
     </ConditionalTooltipOrModal>
