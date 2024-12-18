4f36fef576aa362973e0bd27bbb8eaf87ee5a547
Vadim Matveev <smeea@riseup.net>
2024-12-18 03:42:19 +0300

    fix tda search by rank

--- frontend/src/components/search_forms/components/AnalyzeSearchFormRank.jsx
+++ frontend/src/components/search_forms/components/AnalyzeSearchFormRank.jsx
@@ -7,36 +7,39 @@ const AnalyzeSearchFormRank = ({ value, onChange }) => {
   const { isXWide } = useApp();
   const maxMenuHeight = isXWide ? 500 : 350;
   const name = RANK;
-  const fromOptions = [];
-  const toOptions = [];
+  const steps = ['ANY', '5', '10', '20', '25%', '33%', '50%', '66%', '75%'];
 
-  ['ANY', '5', '10', '15', '25%', '33%', '50%', '66%', '75%'].map((i) => {
-    if (
-      i.toLowerCase() === ANY ||
-      value.to === ANY ||
-      parseInt(i) > value.to ||
-      (value.to.includes('%') && i.includes('%') && i > value.to)
-    ) {
-      fromOptions.push({
-        value: i.toLowerCase(),
-        name: FROM,
-        label: <div className="flex justify-center">{i.toLowerCase() == ANY ? i : `Top ${i}`}</div>,
-      });
-    }
+  const fromOptions = steps
+    .filter((i) => {
+      return (
+        i.toLowerCase() === ANY ||
+        value[TO] === ANY ||
+        !value[TO] ||
+        parseInt(i) > value[TO] ||
+        (value[TO].includes('%') && i.includes('%') && i > value[TO])
+      );
+    })
+    .map((i) => ({
+      value: i.toLowerCase(),
+      name: FROM,
+      label: <div className="flex justify-center">{i.toLowerCase() == ANY ? i : `Top ${i}`}</div>,
+    }));
 
-    if (
-      i.toLowerCase() === ANY ||
-      value.from === ANY ||
-      parseInt(i) < value.from ||
-      (value.from.includes('%') && i.includes('%') && i < value.from)
-    ) {
-      toOptions.push({
-        value: i.toLowerCase(),
-        name: TO,
-        label: <div className="flex justify-center">{i.toLowerCase() == ANY ? i : `Top ${i}`}</div>,
-      });
-    }
-  });
+  const toOptions = steps
+    .filter((i) => {
+      return (
+        i.toLowerCase() === ANY ||
+        value[FROM] === ANY ||
+        !value[FROM] ||
+        parseInt(i) < value[FROM] ||
+        (value[FROM].includes('%') && i.includes('%') && i < value[FROM])
+      );
+    })
+    .map((i) => ({
+      value: i.toLowerCase(),
+      name: TO,
+      label: <div className="flex justify-center">{i.toLowerCase() == ANY ? i : `Top ${i}`}</div>,
+    }));
 
   return (
     <>
@@ -51,7 +54,7 @@ const AnalyzeSearchFormRank = ({ value, onChange }) => {
               isSearchable={false}
               name={name}
               maxMenuHeight={maxMenuHeight}
-              value={fromOptions.find((obj) => obj.value === value.from)}
+              value={fromOptions.find((obj) => obj.value === value[FROM])}
               onChange={onChange}
             />
           </div>
@@ -62,7 +65,7 @@ const AnalyzeSearchFormRank = ({ value, onChange }) => {
               isSearchable={false}
               name={name}
               maxMenuHeight={maxMenuHeight}
-              value={toOptions.find((obj) => obj.value === value.to)}
+              value={toOptions.find((obj) => obj.value === value[TO])}
               onChange={onChange}
             />
           </div>
