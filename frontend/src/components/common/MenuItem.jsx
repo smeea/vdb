46c5ee200a2243d28f02df584a4a3e47acd1488e
Vadim Matveev <smeea@riseup.net>
2025-02-16 11:26:42 +0300

    fix menuitem

--- frontend/src/components/common/MenuItem.jsx
+++ frontend/src/components/common/MenuItem.jsx
@@ -2,7 +2,7 @@ import { Menu } from '@headlessui/react';
 
 const MenuItem = ({ onClick, children }) => {
   return (
-    <Menu.Item className="hover:bg-borderPrimary dark:hover:bg-borderPrimaryDark px-3 py-1.5 whitespace-nowrap">
+    <Menu.Item className="hover:bg-borderPrimary dark:hover:bg-borderPrimaryDark px-3 py-1.5 whitespace-nowrap hover:cursor-pointer">
       {({ active }) => (
         <div
           onClick={onClick}
