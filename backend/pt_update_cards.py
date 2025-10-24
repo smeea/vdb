2fb806af9dda0755976b3233f27d9807cad4df64
Vadim Matveev <smeea@riseup.net>
2025-10-24 20:44:38 +0300

    update v5 sabbat images

--- backend/pt_update_cards.py
+++ backend/pt_update_cards.py
@@ -8,10 +8,10 @@ with (
     open("../frontend/public/data/cardbase_crypt.json", "r") as crypt_file,
     open("../frontend/public/data/cardbase_lib.json", "r") as library_file,
     open(
-        f'../frontend/public/data/cardbase_lib_playtest_{os.environ.get("PLAYTEST_KEY")}.json', "r"
+        f"../frontend/public/data/cardbase_lib_playtest_{os.environ.get('PLAYTEST_KEY')}.json", "r"
     ) as library_playtest_file,
     open(
-        f'../frontend/public/data/cardbase_crypt_playtest_{os.environ.get("PLAYTEST_KEY")}.json',
+        f"../frontend/public/data/cardbase_crypt_playtest_{os.environ.get('PLAYTEST_KEY')}.json",
         "r",
     ) as crypt_playtest_file,
 ):
@@ -27,23 +27,7 @@ with (
 with app.app_context():
     # REMOVE OLD PLAYTEST CARDS FROM DECKS
     # REPLACE CARDID FOR PLAYTEST CARDS AFTER RELEASE
-    changes = {
-        240008: 201729,  # Daf
-        240009: 201730,  # Egi
-        240012: 201777,  # Hel
-        240011: 201732,  # Rog
-        140020: 102305,  # Sha
-        140022: 102306,  # Umb
-        140023: 102299,  # Dea
-        140024: 102303,  # Rai
-        140026: 102293,  # BS
-        140028: 102304,  # Rot
-        140030: 102300,  # Pat
-        140031: 102302,  # Psy
-        140032: 102301,  # Mun
-        140034: 102298,  # Cap
-        140108: 102297,  # Spl
-    }
+    changes = {}
 
     for deck in Deck.query.all():
         new_cards = {}
