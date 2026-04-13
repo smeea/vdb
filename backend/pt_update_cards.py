2d19d6df7a16ac68a13b11a8dddcabac982620e6
Vadim Matveev <smeea@riseup.net>
2026-04-13 23:52:17 +0300

    fix

--- backend/pt_update_cards.py
+++ backend/pt_update_cards.py
@@ -27,30 +27,7 @@ with (
 with app.app_context():
     # REMOVE OLD PLAYTEST CARDS FROM DECKS
     # REPLACE CARDID FOR PLAYTEST CARDS AFTER RELEASE
-    changes = {
-        240005: 201781,  # Bara
-        240004: 201782,  # Djen
-        240007: 201783,  # Haft
-        240001: 201784,  # Madd
-        240006: 201785,  # Nolu
-        240002: 201786,  # Saan
-        140033: 102349,  # Ang Gi
-        140009: 102350,  # Antic
-        140019: 102351,  # Bl Ma
-        140014: 102352,  # Contra
-        140018: 102353,  # Dog Pu
-        140008: 102354,  # Fle Cha
-        140015: 102355,  # Go ge
-        140010: 102356,  # Hunti
-        140016: 102358,  # Phant
-        140011: 102359,  # Rig Bla
-        140006: 102360,  # Sz Ass
-        140005: 102361,  # Sz Bg
-        140013: 102362,  # Tranq
-        140002: 102363,  # Voz Gr
-        140001: 102364,  # Voz Ju
-        140003: 102365,  # Voz Sz
-    }
+    changes = {}
 
     for deck in Deck.query.all():
         new_cards = {}
