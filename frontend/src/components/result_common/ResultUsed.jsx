8a6ff492a31b59bac5a1c21e5918bc06ebe0ceb9
Vadim Matveev <smeea@riseup.net>
2026-08-01 18:12:15 +0300

    fix surplus in crypt/library search

--- frontend/src/components/result_common/ResultUsed.jsx
+++ frontend/src/components/result_common/ResultUsed.jsx
@@ -1,7 +1,7 @@
 import { twMerge } from "tailwind-merge";
 import { useSnapshot } from "valtio";
 import { Tooltip, UsedPopover } from "@/components";
-import { CRYPT, ID, LIBRARY } from "@/constants";
+import { CRYPT, ID, LIBRARY, WISHLIST, LOGIC, SURPLUS_USED, VALUE } from "@/constants";
 import { inventoryStore, sharedStore, useApp, usedStore } from "@/context";
 import { getHardTotal, getSoftMax } from "@/utils";
 
@@ -10,6 +10,7 @@ const ResultUsed = ({ card }) => {
   const { [CRYPT]: inventoryCrypt, [LIBRARY]: inventoryLibrary } = useSnapshot(inventoryStore);
   const { [CRYPT]: usedCrypt, [LIBRARY]: usedLibrary } = useSnapshot(usedStore);
   const { [CRYPT]: sharedCrypt, [LIBRARY]: sharedLibrary } = useSnapshot(sharedStore);
+  const { [WISHLIST]: wishlist } = useSnapshot(inventoryStore);
 
   const used = card[ID] > 200000 ? usedCrypt : usedLibrary;
   const inventory = card[ID] > 200000 ? inventoryCrypt : inventoryLibrary;
@@ -18,9 +19,14 @@ const ResultUsed = ({ card }) => {
   const softUsedMax = getSoftMax(used.soft[card[ID]]);
   const hardUsedTotal = getHardTotal(used.hard[card[ID]]);
   const inInventory = inventory[card[ID]]?.q || 0;
-  const inShared = shared[card[ID]]?.q || 0
-
+  const inShared = shared?.[card[ID]]?.q || 0
   const isInventoryNote = inventory[card[ID]]?.t;
+  const wishlistLogic = wishlist?.[card[ID]]?.[LOGIC];
+  const surplus = wishlistLogic
+        ? wishlistLogic === SURPLUS_USED
+        ? inInventory - (softUsedMax + hardUsedTotal + (wishlist[card[ID]]?.[VALUE] || 0))
+        : inInventory - (wishlist[card[ID]]?.[VALUE] || 0)
+        : inInventory - (softUsedMax + hardUsedTotal);
 
   return (
     <Tooltip placement={isDesktop ? "left" : "bottom"} overlay={<UsedPopover cardid={card[ID]} />}>
@@ -28,7 +34,7 @@ const ResultUsed = ({ card }) => {
         <div
           className={twMerge(
             "mx-1 flex items-center px-0.5",
-            inInventory < softUsedMax + hardUsedTotal &&
+            surplus < 0 &&
               "bg-bgError text-white dark:bg-bgErrorDark dark:text-whiteDark",
           )}
         >
@@ -40,14 +46,14 @@ const ResultUsed = ({ card }) => {
           <div
             className={twMerge(
               "flex basis-2/5 justify-center text-sm",
-              inInventory >= softUsedMax + hardUsedTotal
+              surplus >= 0
                 ? "text-midGray dark:text-midGrayDark"
                 : "text-white dark:text-white",
             )}
           >
-            {inInventory >= softUsedMax + hardUsedTotal
-              ? `+${inInventory - softUsedMax - hardUsedTotal}`
-              : inInventory - softUsedMax - hardUsedTotal}
+            {surplus > 0
+              ? `+${surplus}`
+              : surplus}
           </div>
         </div>
       )}
