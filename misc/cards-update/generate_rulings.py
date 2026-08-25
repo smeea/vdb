b4b832acee5d4a47e4aff90ecf6ba3d6ea5f73d8
Vadim Matveev <smeea@riseup.net>
2026-08-25 21:37:43 +0300

    update rulings

--- misc/cards-update/generate_rulings.py
+++ misc/cards-update/generate_rulings.py
@@ -18,11 +18,12 @@ def parse_rule(rule):
     }
 
 
-with open("rulings/groups.yaml", "r") as groups_file, open(
-    "rulings/references.yaml", "r"
-) as references_file, open("rulings/rulings.yaml", "r") as rulings_file, open(
-    "rulings.json", "w", encoding="utf8"
-) as rulings_output:
+with (
+    open("rulings/groups.yaml", "r") as groups_file,
+    open("rulings/references.yaml", "r") as references_file,
+    open("rulings/rulings.yaml", "r") as rulings_file,
+    open("rulings.json", "w", encoding="utf8") as rulings_output,
+):
     groups_data = yaml.safe_load(groups_file)
     references_data = yaml.safe_load(references_file)
     rulings_data = yaml.safe_load(rulings_file)
@@ -31,6 +32,9 @@ with open("rulings/groups.yaml", "r") as groups_file, open(
 
     # DIRECT CARD RULINGS
     for k, r in rulings_data.items():
+        if k.startswith("G"):
+            continue
+
         [cardid, _] = k.split("|")
         card_rulings = []
 
@@ -42,11 +46,19 @@ with open("rulings/groups.yaml", "r") as groups_file, open(
     # GROUP RULINGS
     for k, cards in groups_data.items():
         for r in rulings_data[k]:
-            rule = parse_rule(r)
+            rule = parse_rule(r["text"]) if type(r) is dict else parse_rule(r)
+
+            overrides = {}
+            if type(r) is dict:
+                for c, o in r["overrides"].items():
+                    [cardid, _] = c.split("|")
+                    overrides[cardid] = parse_rule(o)
 
             for card, target in cards.items():
                 [cardid, _] = card.split("|")
-                card_rule = rule.copy()
+
+                card_rule = overrides[cardid] if cardid in overrides else rule.copy()
+
                 if target:
                     card_rule["text"] = f"{target} {card_rule['text']}"
                 if cardid in all_rulings:
